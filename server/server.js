04868284fef32051814b8eecfc216811ae68c683
PSousa <p4sousa@gmail.com>
2016-04-30 16:18:27 +0100

    tudo necessario! Kappa

--- server/server.js
+++ server/server.js
@@ -1,23 +1,66 @@
 'use strict';
 
 const Hapi = require('hapi');
+var fs = require('fs');
 
 const server = new Hapi.Server();
-server.connection({ port: 3000 });
+server.connection({ port: process.env.PORT || 3000 });
 
 server.route({
     method: 'GET',
     path: '/',
     handler: function (request, reply) {
-        reply('Hello, world!');
+        reply.file('home.html');
     }
 });
 
 server.route({
     method: 'GET',
-    path: '/{name}',
-    handler: function (request, reply) {
-        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    path: '/static/{filename}',
+     handler: function (request, reply) {
+        reply.file('static/' + request.params.filename);
+    }
+});
+
+server.route({
+    method: 'POST',
+    path: '/upload',
+    config: {
+
+        payload: {
+            output: 'stream',
+            parse: true,
+            allow: 'multipart/form-data'
+        },
+
+        handler: function (request, reply) {
+
+             console.log(request);
+            var data = request.payload;
+            if (data.file) {
+                var name = data.file.hapi.filename;
+                var path = __dirname + "/uploads/" + name;
+                var file = fs.createWriteStream(path);
+
+                console.log(path);
+
+                file.on('error', function (err) { 
+                    console.error(err) 
+                });
+
+                data.file.pipe(file);
+
+                data.file.on('end', function (err) { 
+                    var ret = {
+                        filename: data.file.hapi.filename,
+                        headers: data.file.hapi.headers
+                    }
+                    console.log(JSON.stringify(ret));
+                    reply(JSON.stringify(ret));
+                })
+            }
+
+        }
     }
 });
 
