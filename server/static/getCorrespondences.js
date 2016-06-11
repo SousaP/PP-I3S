6057b62be6a78d360f4761a7faac523fc168e80a
Mouzician <mfilipe_13@hotmail.com>
2016-06-11 18:59:36 +0100

    Falta Guardar o ficheiro

--- server/static/getCorrespondences.js
+++ server/static/getCorrespondences.js
@@ -2,14 +2,13 @@
 	var fs = require('fs');
 	var path = require('path');
 	var specie = $('#slElem').val();
-	var filePath = path.join(__dirname, 'resources/fasta/' + specie + '_fasta.txt');
-	var fileName = path.join(__dirname, 'resources/temp/modified_fasta_temp.txt');
+	var filePath = 'resources/fasta/' + specie + '_fasta.txt';
+	var fileName = 'resources/temp/modified_fasta_temp.txt';
 	var def = genes.split('\n');
 
 	$(function() {
 	$.get('fasta/' + specie + '_fasta.txt', function(data) {
 	var output = "# Temporary Fasta file\n\n";
-			console.log(data);
 			var lines = data.split('>');
 			console.log(lines);
 			for(var line = 0; line < lines.length; line++){
@@ -40,9 +39,7 @@
 
 	$(function() {
 	$.get('BIOGRID/' + interactome, function(data) {
-	var output = "# Temporary Fasta file\n\n";
-			var output = gene + '\n';
-			console.log(data);
+			var output = "# Temporary Fasta file\n\n" + gene + '\n';
 			var lines = data.split('\n');
 			for(var line = 0; line < lines.length; line++){
 				var temp = lines[line];
@@ -51,15 +48,41 @@
 				}
 				else if(temp.indexOf(gene) > -1){
 					var sep = temp.split('\t')
-					output = output + sep[1] + '\n';
+					if(sep[1] == gene){
+						output = output + sep[0] + '\n';
+					}
+					else{
+						output = output + sep[1] + '\n';
+					}
 				}
 			}
 			
 			var element = document.createElement("pre");
 			element.textContent = output;
 			document.getElementById('s1').appendChild(element);
+			
+			var specie = $('#slElem').val();
+			
+			$.get('fasta/' + specie + '_fasta.txt', function(data) {
+			var def = output.split('\n');
+			var fasta = "# Temporary Fasta file\n\n";
+			var lines = data.split('>');
+			for(var line = 0; line < lines.length; line++){
+				var temp = lines[line];
+				if(temp.indexOf("#") > -1 || temp == ""){
 					
-			createFastaFile(output);
+				}
+				for(var i = 0; i < def.length; i++){
+					if ((temp.indexOf(def[i]) >= 0 || temp.toLowerCase().indexOf(def[i]) >= 0 
+					|| temp.toUpperCase().indexOf(def[i]) >= 0) && def[i] != ""){
+						fasta += ">" + temp;
+					}
+				}
+				
+			}
+			console.log(fasta);
+			//createFastaFile(output);
+			});
 	});
 });
 
