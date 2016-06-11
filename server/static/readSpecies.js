89a739a51ca3f94ed58c3c51523f65a7c72fdb57
Mouzician <mfilipe_13@hotmail.com>
2016-06-11 15:51:58 +0100

    ads

--- server/static/readSpecies.js
+++ server/static/readSpecies.js
@@ -1,11 +1,20 @@
-
-$(function() {
+var speciesData = function () {
 	$.get('resources/species_list.txt', function(data) {
 		var lines = data.split('\n');
+		var array = [];
+		var item = {};
 		for(var line = 0; line < lines.length; line++){
-			$('#slElem').append("<option value=" + lines[line].split(' ').join('_') + ">" + lines[line] + "</option>");
-			$('#slElem2').append("<option value=" + lines[line].split(' ').join('_') + ">" + lines[line] + "</option>");
+			item['id'] = lines[line].split(' ').join('_');
+			item['text'] = lines[line];
+			array.push(item);
+			item = {};
 		}
+		return array;
 	});
+}
+
+$(function() {
+	var ret = speciesData();
+	console.log(ret);
 });
 
